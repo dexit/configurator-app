07def5326fc6897cf1452e8f86c5df893fe43d22
DESKTOP-UQLP1Q2\Łukasz <lukkupis@gmail.com>
2019-04-11 20:03:34 +0200

    fix router: choosing a language - in progress

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
 import Configurator from './configurator/Configurator';
 
-const Rtr = props => {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route
-          path={'/' + props.t('configurator.routeCategoryName') + '/:category'}
-          component={Configurator}
-        />
-        <Route
-          path={'/' + props.t('configurator.routeSummaryName')}
-          component={Configurator}
-        />
-        <Route path="/" component={Configurator} />
-      </Switch>
-    </Router>
-  );
-};
+import translations from './translations';
+
+class Rtr extends Component {
+  getLanguagesString() {
+    let languages = [];
+
+    for (const key in translations) {
+      languages.push(key);
+    }
+
+    const languagesString = languages.join('|');
+
+    return languagesString;
+  }
+
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route
+            path={`/:lng(${this.getLanguagesString()})`}
+            component={Configurator}
+          />
+          <Route
+            path={
+              '/' +
+              this.props.t('configurator.routeCategoryName') +
+              '/:category'
+            }
+            component={Configurator}
+          />
+          <Route
+            path={'/' + this.props.t('configurator.routeSummaryName')}
+            component={Configurator}
+          />
+          <Route path="/" component={Configurator} />
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 export default withTranslation()(Rtr);
