30753c45e35adfc0a399142069ffee4aa24c5493
DESKTOP-UQLP1Q2\Łukasz <lukkupis@gmail.com>
2019-04-17 18:52:03 +0200

    Updates

---
+++ precache-manifest.2262630c864e9abac233210090d91e08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1fdd868ecb50fb8cc2a",
+    "url": "/configurator-app/static/js/runtime~main.b73bb04c.js"
+  },
+  {
+    "revision": "9d8491f50a9d31630158",
+    "url": "/configurator-app/static/js/main.5c8fc3ae.chunk.js"
+  },
+  {
+    "revision": "3ed25aa673feb263a4a6",
+    "url": "/configurator-app/static/js/2.9fd0585b.chunk.js"
+  },
+  {
+    "revision": "9d8491f50a9d31630158",
+    "url": "/configurator-app/static/css/main.0605b076.chunk.css"
+  },
+  {
+    "revision": "4d2079701dcdf71ae84f370fd4522403",
+    "url": "/configurator-app/index.html"
+  }
+];
